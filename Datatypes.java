61b021522835d742f06376bc4b5821bef2fa5406
divyasundarraj <114854114+divyasundarraj@users.noreply.github.com>
2022-10-06 18:24:01 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class Datatypes
+{
+public static void main(String[] args)
+{
+int a=20;
+System.out.println("output="+a);
+float b=1.2f;
+System.out.println("output="+b);
+char c='x';
+System.out.println("output="+c);
+boolean d=true;
+System.out.println("output="+d);
+byte e=120;
+System.out.println("output="+e);
+short f=32;
+System.out.println("output="+f);
+double g=12.0;
+System.out.println("output="+g);
+String h="divyasundarraj";
+System.out.println("output="+h);
+}
+}
